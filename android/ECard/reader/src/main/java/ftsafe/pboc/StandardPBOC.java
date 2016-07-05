8e38978c4782f02968c3f1cb90a876a29c01892f
qingyuan <qingyuan@qingyuan-PC>
2016-07-05 17:01:06 +0800

    添加图片信息

--- android/ECard/reader/src/main/java/ftsafe/pboc/StandardPBOC.java
+++ android/ECard/reader/src/main/java/ftsafe/pboc/StandardPBOC.java
@@ -1,9 +1,25 @@
 package ftsafe.pboc;
 
+import ftsafe.reader.Reader;
+
 /**
  * Created by qingyuan on 16/7/5.
  */
 public abstract class StandardPBOC {
-    private static Class<?>[][] applet = { };
+    private static Class<?>[][] applets = { };
+
+    public static void readerCard(Reader reader, Class<?> applet) throws Exception {
+        if (reader == null || applet == null)
+            return;
+        // 卡片上电
+        reader.powerOn();
+
+        final StandardPBOC app = (StandardPBOC) applet.newInstance();
+
+        // 卡片下电
+        reader.powerOff();
+    }
+
+
 
 }
